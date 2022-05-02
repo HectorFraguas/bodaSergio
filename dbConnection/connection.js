c7d6c0a485f7a77c2343449b52fa3ffe66dc3886
Hector Fraguas Iglesias <fraguas14@gmail.com>
2022-05-02 11:59:29 +0200

    Instalado cors

--- dbConnection/connection.js
+++ dbConnection/connection.js
@@ -1,10 +1,7 @@
 var mongoose = require("mongoose");
 
-
-
-
 const connection = () => {
-    mongoose.connect(`mongodb+srv://bodaSergioMonica:${process.env.password || "bodasergio"}@cluster0.8a9bw.mongodb.net/${process.env.dbName || "boda"}?retryWrites=true&w=majority`)
+    mongoose.connect(`mongodb+srv://bodaSergioMonica:${process.env.password}@cluster0.8a9bw.mongodb.net/${process.env.dbName}?retryWrites=true&w=majority`)
     console.log("Conectado")
 
 }
