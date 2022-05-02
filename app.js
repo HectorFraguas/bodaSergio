ba67c199e7f78ea6affb7468ff5a0ee89b7c4604
Hector Fraguas Iglesias <fraguas14@gmail.com>
2022-05-02 11:51:15 +0200

    Instalado cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors")
 
 var bodaRouter = require('./routes/boda');
 
@@ -21,6 +22,7 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/boda', bodaRouter);
 
--- dbConnection/connection.js
+++ dbConnection/connection.js
@@ -1,7 +1,10 @@
 var mongoose = require("mongoose");
 
+
+
+
 const connection = () => {
-    mongoose.connect(`mongodb+srv://bodaSergioMonica:${process.env.password}@cluster0.8a9bw.mongodb.net/${process.env.dbName}?retryWrites=true&w=majority`)
+    mongoose.connect(`mongodb+srv://bodaSergioMonica:${process.env.password || "bodasergio"}@cluster0.8a9bw.mongodb.net/${process.env.dbName || "boda"}?retryWrites=true&w=majority`)
     console.log("Conectado")
 
 }
